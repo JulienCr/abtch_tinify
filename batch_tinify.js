06b3d196909b456c92107bc067eea2c48355c3f7
Julien Cruau <julien.cruau@bedrockstreaming.com>
2023-10-30 19:38:24 +0100

    move treated images to ok folder

--- batch_tinify.js
+++ batch_tinify.js
@@ -28,17 +28,37 @@ async function optimizeImage(imagePath) {
 
 async function process(filePath, destDir) {
     const fileExtension = path.extname(filePath).toLowerCase();
+
+    if (fs.lstatSync(filePath).isDirectory()) {
+        return;
+    }
+
     if (![".jpg", ".png", ".jpeg"].includes(fileExtension)) {
         console.log(`Le fichier ${filePath} n'est pas une image valide. Il sera ignoré.`);
         return;
     }
 
     console.log("Traitement de l'image: ", filePath);
+
+    try {
     const resizedFilePath = await resizeImage(filePath);
     await optimizeImage(resizedFilePath);
 
+    // Copy the file to the output directory
     const command = os.platform() === 'win32' ? `copy ${resizedFilePath} ${path.join(destDir, path.basename(filePath))}` : `cp ${resizedFilePath} ${path.join(destDir, path.basename(filePath))}`;
     execSync(command);
+
+    // Move original file to ./ok/
+    const okDir = "input/ok";
+    if (!fs.existsSync(okDir)) {
+        fs.mkdirSync(okDir);
+    }
+    const okFilePath = path.join(okDir, path.basename(filePath));
+    fs.renameSync(filePath, okFilePath);
+
+    } catch (err) {
+        console.error("Erreur lors du traitement de l'image ", filePath, err);
+    }
 }
 
 async function main() {
